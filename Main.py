92eefac8c9f9b87bbdce1ee93b6511db3bd0acad
mauricesinon123-cmyk <maurice.sinon123@gmail.com>
2025-12-25 13:20:28 +0100

    Enable debug mode based on FLASK_DEBUG variable
    Added environment variable support for debug mode.

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 from PythonProject import create_app
+import os
 
-#Creeërt de flask applicatie
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+    app.run(debug=debug_mode)
 
